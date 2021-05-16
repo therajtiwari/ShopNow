f8fdfdf9ec01de6775516082bad3700700d4defa
Raj <therajtiwari254@gmail.com>
2021-05-16 05:42:16 +0530

    added backend files

---
+++ backend/data/products.js
@@ -0,0 +1,118 @@
+const products = [
+  {
+    name: "iphone 12 Pro Max",
+    id: 1,
+    image: "/images/apple_iphone_12.jpg",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic est atque ratione. Totam nesciunt autem minus obcaecati ullam sunt rem.",
+    brand: "apple",
+    category: "Electronics",
+    sub_category: "Mobile phones",
+    price: 130000,
+    avalaibleStock: 24,
+    rating: 4,
+    reviewCount: 10,
+  },
+
+  {
+    name: "Samsung Galaxy S20",
+    id: 2,
+    image: "/images/samsung_galaxy_s20.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "Samsung",
+    category: "Electronics",
+    sub_category: "Mobile phones",
+    price: 80000,
+    avalaibleStock: 15,
+    rating: 4.6,
+    reviewCount: 15,
+  },
+  {
+    name: "Sony PS5",
+    id: 3,
+    image: "/images/sony_ps5.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "Sony",
+    category: "Electronics",
+    sub_category: "Gaming Consoles",
+    price: 45000,
+    avalaibleStock: 1,
+    rating: 5,
+    reviewCount: 5,
+  },
+  {
+    name: "Apple Airpods Pro",
+    id: 4,
+    image: "/images/apple_airpods_pro.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "APple",
+    category: "Electronics",
+    sub_category: "Earphones and Headphones",
+    price: 20000,
+    avalaibleStock: 100,
+    rating: 3.8,
+    reviewCount: 50,
+  },
+  {
+    name: "Sony Headphone",
+    id: 5,
+    image: "/images/sony_headphone.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "Sony",
+    category: "Electronics",
+    sub_category: "Earphones and Headphones",
+    price: 30000,
+    avalaibleStock: 24,
+    rating: 3.8,
+    reviewCount: 20,
+  },
+  {
+    name: "MSI RTX 3090",
+    id: 6,
+    image: "/images/msi_rtx_3090.jpeg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "MSI",
+    category: "Electronics",
+    sub_category: "Graphic Cards",
+    price: 150000,
+    avalaibleStock: 5,
+    rating: 5,
+    reviewCount: 10,
+  },
+  {
+    name: "Sony Alpha 5",
+    id: 7,
+    image: "/images/sony_alpha_camera.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "Sony",
+    category: "Electronics",
+    sub_category: "Cameras",
+    price: 200000,
+    avalaibleStock: 10,
+    rating: 4.5,
+    reviewCount: 5,
+  },
+  {
+    name: "Yamaha fs80c",
+    id: 8,
+    image: "/images/yamaha_fs80c_guitar.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aspernatur consequatur voluptatum nam impedit praesentium illum dolore reprehenderit asperiores sed.",
+    brand: "Yamaha",
+    category: "Music",
+    sub_category: "Guitars",
+    price: 8000,
+    avalaibleStock: 2,
+    rating: 4.6,
+    reviewCount: 9,
+  },
+];
+
+// export default products;
+module.exports = products;
