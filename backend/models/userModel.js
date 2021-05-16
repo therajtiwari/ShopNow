38380c632131da806a65c17e67c435c5cdea4e23
Raj <therajtiwari254@gmail.com>
2021-05-16 14:42:54 +0530

    added model schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNo: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
+
+const User = moongoose.model("User", userSchema);
+
+export default User;
